a70dae70b0490a9b1509e0b0c1d473f4aabb39ad
Enes UTKU <ensutku@hotmail.com>
2024-04-29 22:44:25 +0300

    Initial Commit

---
+++ ChatWithMe/ChatWithMeApp.swift
@@ -0,0 +1,12 @@
+// Created by Enes UTKU
+
+import SwiftUI
+
+@main
+struct ChatWithMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChatWithMe/ContentView.swift
@@ -0,0 +1,19 @@
+// Created by Enes UTKU
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
